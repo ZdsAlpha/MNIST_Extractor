16f38f0d5ce072bb1252b647080e668168e51635
ZDS Alpha <abzsoft999@hotmail.com>
2018-07-25 16:50:31 +0500

    Uploaded Code

---
+++ MNISTExtractor/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+
+namespace MNISTExtractor
+{
+    class Program
+    {
+        static readonly string DefaultOutputDirectory = "output\\";
+        static void Main(string[] args)
+        {
+            string imagesFile = Input("Enter images filename: ");
+            string labelsFile = Input("Enter labels filename: ");
+            string outputDir = Input("Enter output directory (optional): ");
+            if (string.IsNullOrWhiteSpace(outputDir)) outputDir = DefaultOutputDirectory;
+            if (!File.Exists(imagesFile ) || !File.Exists(labelsFile))
+            {
+                Print("File(s) missing.");
+                Pause();
+                return;
+            }
+            BinaryReader imageReader = new BinaryReader(new FileStream(imagesFile, FileMode.Open));
+            BinaryReader labelReader = new BinaryReader(new FileStream(labelsFile, FileMode.Open));
+            if (ReadInt(imageReader) != 2051 || ReadInt(labelReader) != 2049)
+            {
+                Print("Invalid magic number.");
+                Pause();
+                imageReader.Dispose();
+                labelReader.Dispose();
+                return;
+            }
+            int count1 = ReadInt(imageReader);
+            int count2 = ReadInt(labelReader);
+            if (count1 != count2)
+            {
+                Print("Images and Labels count mismatched.");
+                Pause();
+                imageReader.Dispose();
+                labelReader.Dispose();
+                return;
+            }
+            Directory.CreateDirectory(outputDir);
+            for (int i = 0; i <= 9; i++)
+                Directory.CreateDirectory(Path.Combine(outputDir, i.ToString()));
+            Print("Starting conversion...");
+            int count = count1 = count2;
+            int rows = ReadInt(imageReader);
+            int columns = ReadInt(imageReader);
+            for(int i = 0; i < count; i++)
+            {
+                byte label = labelReader.ReadByte();
+                Bitmap image = new Bitmap(columns, rows);
+                Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+                System.Drawing.Imaging.BitmapData data =
+                    image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                    System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                IntPtr ptr = data.Scan0;
+                int bytes = Math.Abs(data.Stride) * image.Height;
+                byte[] rgbValues = new byte[bytes];
+                System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+                for (int j = 0; j < rows * columns; j++)
+                    rgbValues[j * 3] = rgbValues[j * 3 + 1] = rgbValues[j * 3 + 2] = imageReader.ReadByte();
+                System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+                image.UnlockBits(data);
+                string name = Path.Combine(outputDir, label.ToString(), i.ToString() + ".png");
+                image.Save(name);
+                image.Dispose();
+                Print(name);
+            }
+        }
+        static int ReadInt(BinaryReader reader)
+        {
+            byte[] value = reader.ReadBytes(4);
+            if (BitConverter.IsLittleEndian) Array.Reverse(value);
+            return BitConverter.ToInt32(value, 0);
+        }
+        static void Print(string message) => Console.WriteLine(message);
+        static string Input(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        static void Pause() => Console.ReadKey();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# MNIST_Extractor
-Extract images from MNIST dataset
+# MNIST Extractor
+A C# console application to extract images from MNIST dataset and save it to 10 directories (0 to 9) each crossponding to its class.
+# How to use
+- Download [MNIST Dataset](http://yann.lecun.com/exdb/mnist/ "MNIST Dataset")
+- Put and Extract it in executable directory
+- Run Extractor
+- Enter Images and Labels File (e.g `t10k-images.idx3-ubyte` and `t10k-labels.idx1-ubyte` for training and `train-images.idx3-ubyte` and `train-labels.idx1-ubyte` for testing)
+- Enter output directory
+# Output
+It will generate a directory and create 10 directories name ranging from 0 to 9. It will extract each image from MNIST dataset and will place it into the directory it is crossponds to.
